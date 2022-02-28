96ec1ebb5612acfed4b784478ca6473ddb2235eb
seekseep <seekseep@gmail.com>
2022-02-28 20:20:02 +0900

    [ADD] mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import CssBaseline from '@mui/material/CssBaseline';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
